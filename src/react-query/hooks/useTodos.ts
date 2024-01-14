7d351416249083e81be00e5fe33ebc945e8c3fae
LuckyBelieve <luckybelieve10@gmail.com>
2024-01-14 14:18:33 +0200

    implement todoService

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,21 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIclient from "../services/apiClient";
+import todoService, { Todo } from "../services/todoService";
 
-const apiClient = new APIclient<Todo>('/todos');
 
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
 
 const useTodos = () =>
   useQuery<Todo[], Error>({
     queryKey:CACHE_KEY_TODOS,
-    queryFn: apiClient.getAll,
+    queryFn: todoService.getAll,
     staleTime: 10 * 1000
   });
 
